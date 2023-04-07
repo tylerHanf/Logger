9d8e973fc2d345f5976130daed6875562ca94bea
tylerHanf <tyler.hanf21@gmail.com>
2023-04-07 10:54:47 -0400

    Moved to generic log function where destination can be specied earlier

--- include/Logger.h
+++ include/Logger.h
@@ -3,12 +3,19 @@
 #include <stdarg.h>
 #include <string>
 
+enum LogDest
+{
+	DEST_FILE = 0x1,
+	DEST_CONSOLE = 0x2,
+};
+
 class Logger {
 public:
-	static void LogPrint(const char* msg, ...);
-	static void LogFile(const char* msg, ...);
-	static void InitLog(const char* logPath);
+	static void Log(const char* msg, ...);
+	static void InitLog(const char* logPath, int destFlags=DEST_CONSOLE);
+	static void SetLogDest(int destFlag);
 
 private:
-	static void Log(const char* msg, bool toFile, va_list args);
+	static void LogPrint(const char* msg);
+	static void LogFile(const char* msg);
 };
--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,43 +1,53 @@
 #include "Logger.h"
 #include <iostream>
+#include <sstream>
+
+// Todo: Make this configurable at some point
+#define MAX_BUFF_SIZE 512
 
 static const char* logPath;
 static std::FILE* fp;
 
-void Logger::LogPrint(const char* msg, ...) {
-	va_list args;
-	va_start(args, msg);
-	Logger::Log(msg, false, args);
+static bool toFile = false;
+static bool toConsole = false;
+
+void Logger::SetLogDest(int destFlags)
+{
+	toFile = destFlags & DEST_FILE;
+	toConsole = destFlags & DEST_CONSOLE;
 }
 
-void Logger::LogFile(const char* msg, ...) {
-	va_list args;
-	va_start(args, msg);
-	Logger::Log(msg, true, args);
-}
 
-void Logger::InitLog(const char* logPath) {
+void Logger::InitLog(const char* logPath, int destFlags) {
+	SetLogDest(destFlags);
+
 	fp = fopen(logPath, "w");
 	if (fp != NULL) {
 		printf("Initialized Logger\n");
 	}
 
 	else {
-		printf("ERROR: Failed to initialize Logger\n");
+		printf("ERROR: Failed to create log file, bad file path\n");
 	}
 }
 
-/*
+/**
 * Main logic for handling logging args
 * Format specifiers:
 *		-d = int
 *       -f = double
 *       -c = char
 *       -s = string
+*
+* @todo Move entrance call to macro to allow for error location information
 */
-void Logger::Log(const char* msg, bool toFile, va_list args) {
+void Logger::Log(const char* msg, ...) {
+	std::stringstream stream;
+	char buff[MAX_BUFF_SIZE];
+	int next = 0;
 
-	std::FILE* o_stream = toFile ? fp : stdout;
+	va_list args;
+	va_start(args, msg);
 
 	bool formatModifier = false; // true if the next char is for defining a var format
 	while (*msg != '\0') {
@@ -49,34 +59,34 @@ void Logger::Log(const char* msg, bool toFile, va_list args) {
 			case 'd':
 			{
 				int i = va_arg(args, int);
-				fprintf(o_stream, "%d", i);
+				next += sprintf(&buff[next], "%d", i);
 				break;
 			}
 
 			case 'f':
 			{
 				double f = va_arg(args, double);
-				fprintf(o_stream, "%f", f);
+				next += sprintf(&buff[next], "%f", f);
 				break;
 			}
 
 			case 'c':
 			{
 				int c = va_arg(args, int);
-				fprintf(o_stream, "%c", c);
+				next += sprintf(&buff[next], "%c", c);
 				break;
 			}
 
 			case 's':
 			{
 				char* s = va_arg(args, char*);
-				fprintf(o_stream, "%s", s);
+				next += sprintf(&buff[next], "%s", s);
 				break;
 			}
 
 			default:
 			{
-				fprintf(o_stream, "\nERROR: unrecognizable printf format specifier\n");
+				next += sprintf(&buff[next], "\nERROR: unrecognizable printf format specifier\n");
 				break;
 			}
 
@@ -85,10 +95,32 @@ void Logger::Log(const char* msg, bool toFile, va_list args) {
 		}
 
 		else {
-			fprintf(o_stream, "%c", *msg);
+			next += sprintf(&buff[next], "%c", *msg);
 		}
 
 		++msg;
+
+		// Make sure we don't write past end of buffer
+		if (next >= MAX_BUFF_SIZE-1)
+			break;
 	}
-	fprintf(o_stream, "\n");
+	sprintf(&buff[next], "\n");
+
+	if (toConsole)
+	{
+		LogPrint(&buff[0]);
+	}
+
+	if (toFile)
+	{
+		LogFile(&buff[0]);
+	}
+}
+
+inline void Logger::LogPrint(const char* msg) {
+	std::cout << msg << std::endl;
+}
+
+inline void Logger::LogFile(const char* msg) {
+	fprintf(fp, "%s", &msg[0]);
 }
